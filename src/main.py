f8c853f3538dd21701b3905f0f773ad33b667d6e
Alphactory <aedan.e.taylor@gmail.com>
2020-04-29 23:21:14 -0400

    polish

--- src/main.py
+++ src/main.py
@@ -1,21 +1,37 @@
 from src.Triangle import *
 from src.DecisionNode import *
+
+
+# neither 1 nor 5 had any answers
 t = Triangle(3)
+
+# construct the tree
 root = DecisionNode(t)
 root.discover_children()
-t2 = Triangle(1)
-root2 = DecisionNode(t2)
-root2.discover_children()
 finished_traversals = root.get_finished_traversals()
+
+# now display solutions and their scores (only 14, no pruning)
 moved_coin = None
+
+
+def prettyprint(array):
+    result = ""
+    for move in traversal:
+        result += ", ("+str(move[0])+" - "+str(move[2])+")"
+    return result[2:]
+
+
 for traversal in finished_traversals:
     traversal.reverse()
-    print(traversal)
+    print(prettyprint(traversal))
     score = 0
     for move in traversal:
         if moved_coin != move[0]:
             score += 1
         moved_coin = move[2]
-    print(score)
+    print("This solution scores "+str(score)+"\n")
 
 
+# I should have written this in java
+
+# no, wait, I should have worked on my calculus instead of doing this.
